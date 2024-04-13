76e13e802804b86982a7fe3e0093b8b9b8fbef79
Themystocles <themystocles@outlook.com>
2024-04-13 10:59:20 -0300

    trabalhando com *ngFor

--- Front/ProEventos_APP/src/app/eventos/eventos.component.ts
+++ Front/ProEventos_APP/src/app/eventos/eventos.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.scss']
 })
-export class EventosComponent {
+export class EventosComponent{
+
+  public eventos: any = [{Tema: 'Angular', Local: 'belo horizonte'},
+  {Tema: 'C#', Local: 'Fortaleza'},
+  {Tema: 'GIT', Local: 'São paulo'}
+  ]
 
 }
